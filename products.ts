fd831987f8147a8c24b339f6bbf24260344a1973
?ngel ?lvarez <angel@nan-tic.com>
2017-11-02 12:59:41 +0100

    initial commit

---
+++ products.ts
@@ -0,0 +1,11 @@
+/**
+ * Products interface for product
+ * DO NOT MODIFY THIS FILE, EXTEND THE INTERFACE INSTEAD
+*/
+
+export interface Products {
+    id: number;
+    name: string;
+    rec_name: string;
+    codes_number?: string[];
+}
